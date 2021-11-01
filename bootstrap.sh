4090fa7102efc1a16054eb6b3195ba1df60e58b6
Brian Lyttle <brian+github@killaloo.org>
2021-10-31 21:46:25 -0400

    Simpler bootstrap so everything is done via brew.

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+
+exists ()
+{
+  command -v "$1" > /dev/null 2>&1
+}
+
+if ! exists brew; then
+  echo "Installing homebrew"
+  # Install homebrew
+#   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if ! exists brew; then
+    echo "Installing ansible via brew"
+    brew install ansible
+fi
+
+echo "Machine is ready to run playbooks"
